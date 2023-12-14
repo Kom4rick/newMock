125bc83b4c68eb3ca062002a7cafad78c3d0e020
unknown <forsenboy123@hmail.com>
2023-12-14 17:55:50 +0300

    second commit

--- src/main/java/com/example/newMock/Model/RequestDTO.java
+++ src/main/java/com/example/newMock/Model/RequestDTO.java
@@ -10,47 +10,6 @@ import lombok.*;
 public class RequestDTO {
     private String rqUID;
     private String clientId;
-
-//    public String getRqUID() {
-//        return rqUID;
-//    }
-//
-//    public void setRqUID(String rqUID) {
-//        this.rqUID = rqUID;
-//    }
-//
-//    public String getClientId() {
-//        return clientId;
-//    }
-//
-//    public void setClientId(String clientId) {
-//        this.clientId = clientId;
-//    }
-//
-//    public String getAccount() {
-//        return account;
-//    }
-//
-//    public void setAccount(String account) {
-//        this.account = account;
-//    }
-//
-//    public String getOpenDate() {
-//        return openDate;
-//    }
-//
-//    public void setOpenDate(String openDate) {
-//        this.openDate = openDate;
-//    }
-//
-//    public String getCloseDat() {
-//        return closeDat;
-//    }
-//
-//    public void setCloseDat(String closeDat) {
-//        this.closeDat = closeDat;
-//    }
-
     private String account;
     private String openDate;
     private String closeDat;
